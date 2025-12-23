45b6abc984a47fde0381ede6594ce571426a12e6
ranjit-dey <ranjitdey05265@gmail.com>
2025-12-23 23:41:32 +0530

    add admin layout

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
+import Blog from './components/Blog'
+import AddBlog from './pages/admin/AddBlog'
+import Comments from './pages/admin/Comments'
+import Dashboard from './pages/admin/Dashboard'
+import Layout from './pages/admin/Layout'
+import ListBlog from './pages/admin/ListBlog'
+import Blogs from './pages/Blogs'
+import Home from './pages/Home'
+import Login from './components/admin/Login'
+
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route path="/blogs/:id" element={<Blog />} />
+                <Route path="/admin" element={true ? <Layout/>  : <Login/>}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="addBlog" element={<AddBlog />} />
+                    <Route path="listBlog" element={<ListBlog />} />
+                    <Route path="comments" element={<Comments />} />
+                </Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default App
