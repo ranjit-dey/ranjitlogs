ce3b487da07610177303a4d190ad13ab695e3096
ranjit-dey <ranjitdey05265@gmail.com>
2025-12-26 22:49:06 +0530

    update adminRoutes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,7 +2,5 @@ import express from 'express'
 import adminLogin from '../controllers/adminController.js'
 
 const adminRouter = express.Router()
-
 adminRouter.post('/login', adminLogin)
-
 export default adminRouter
